5440fd257515e5c5498c67ba3e8a0cdf07c2325f
Matheus Sabadin <matheushs15@hotmail.com>
2022-10-22 03:19:49 -0300

    First clena build

--- RevitGeometryUtils/Properties/DirectShapeCreator.cs
+++ RevitGeometryUtils/Properties/DirectShapeCreator.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using VZTagMaterial.Standard;
-using static VZTagMaterial.Auxiliar.RoomCumulativeMaterial;
 
 namespace VZTagMaterial.Auxiliar
 {
